1e4b71428f9b2f741c908fdbb3a82df1f07f6fb0
Morgan Larsson <morgan.larsson@qualisys.se>
2019-07-08 16:12:19 +0200

    Updated prebuilt to UE 4.22.3 binaries

---
+++ Prebuilt/Qualisys/QTMConnectLiveLink/Source/QTMConnectLiveLink/Private/QTMConnectLiveLink.cpp
@@ -0,0 +1,20 @@
+// QTM Connect For Unreal. Copyright 2018-2019 Qualisys
+//
+#include "QTMConnectLiveLink.h"
+
+#define LOCTEXT_NAMESPACE "FQTMConnectLiveLinkModule"
+
+void FQTMConnectLiveLinkModule::StartupModule()
+{
+    // This code will execute after your module is loaded into memory; the exact timing is specified in the .uplugin file per-module
+}
+
+void FQTMConnectLiveLinkModule::ShutdownModule()
+{
+    // This function may be called during shutdown to clean up your module.  For modules that support dynamic reloading,
+    // we call this function before unloading the module.
+}
+
+#undef LOCTEXT_NAMESPACE
+
+IMPLEMENT_MODULE(FQTMConnectLiveLinkModule, QTMConnectLiveLink)
---
+++ Prebuilt/Qualisys/QTMConnectLiveLink/Source/QTMConnectLiveLink/Public/QTMConnectLiveLinkSource.h
@@ -0,0 +1,91 @@
+// QTM Connect For Unreal. Copyright 2018-2019 Qualisys
+//
+#pragma once
+
+#include "ILiveLinkSource.h"
+#include "MessageEndpoint.h"
+#include "IMessageContext.h"
+#include "HAL/ThreadSafeBool.h"
+
+#include "Networking.h"
+#include "Sockets.h"
+#include "SocketSubsystem.h"
+#include <memory>
+
+class ILiveLinkClient;
+class CRTProtocol;
+
+class QTMConnectLiveLinkSettings
+{
+public:
+    FString IpAddress;
+    unsigned short Port;
+};
+
+class QTMCONNECTLIVELINK_API FQTMConnectLiveLinkSource : public ILiveLinkSource, public FRunnable
+{
+public:
+
+	FQTMConnectLiveLinkSource(const QTMConnectLiveLinkSettings& settings);
+
+	virtual ~FQTMConnectLiveLinkSource();
+
+	// Begin ILiveLinkSource Interface
+	
+	virtual void ReceiveClient(ILiveLinkClient* InClient, FGuid InSourceGuid) override;
+
+	virtual bool IsSourceStillValid() override;
+
+	virtual bool RequestSourceShutdown() override;
+
+	virtual FText GetSourceType() const override { return SourceType; };
+	virtual FText GetSourceMachineName() const override { return SourceMachineName; }
+	virtual FText GetSourceStatus() const override { return SourceStatus; }
+
+	// End ILiveLinkSource Interface
+
+	// Begin FRunnable Interface
+
+	virtual bool Init() override { return true; }
+	virtual uint32 Run() override;
+
+    void CreateLiveLinkSubjects();
+
+    void ClearSubjects();
+
+    void Start();
+	virtual void Stop() override;
+	virtual void Exit() override { }
+
+	// End FRunnable Interface
+
+	void HandleReceivedData(TSharedPtr<TArray<uint8>, ESPMode::ThreadSafe> ReceivedData);
+
+private:
+    QTMConnectLiveLinkSettings Settings;
+
+	ILiveLinkClient* Client;
+
+	// Our identifier in LiveLink
+	FGuid SourceGuid;
+
+	FMessageAddress ConnectionAddress;
+
+	FText SourceType;
+	FText SourceMachineName;
+	FText SourceStatus;
+	
+	// Threadsafe Bool for terminating the main thread loop
+	FThreadSafeBool Stopping;
+	//
+	// Thread to run socket operations on
+	FRunnableThread* Thread;
+	//
+	//// Name of the sockets thread
+	FString ThreadName;
+
+	// List of subjects we've already encountered
+	TArray<FName> EncounteredSubjects;
+
+    std::shared_ptr<CRTProtocol> mRTProtocol;
+};
