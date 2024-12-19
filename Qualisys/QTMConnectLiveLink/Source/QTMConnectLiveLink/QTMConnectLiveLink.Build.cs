51f813f76feea3945fead4b74d4de2cfe7c86fca
johannes.gotlen <johannes.gotlen@qualisys.se>
2024-12-19 16:48:36 +0100

    removed unused build code

--- Qualisys/QTMConnectLiveLink/Source/QTMConnectLiveLink/QTMConnectLiveLink.Build.cs
+++ Qualisys/QTMConnectLiveLink/Source/QTMConnectLiveLink/QTMConnectLiveLink.Build.cs
@@ -5,33 +5,11 @@ using UnrealBuildTool;
 
 public class QTMConnectLiveLink : ModuleRules
 {
-    string GetTargetPlatformPathName(ReadOnlyTargetRules Target)
-    {
-#if UE_5_0_OR_LATER
-        return "Win64";
-#else
-        return Target.Platform == UnrealTargetPlatform.Win32 ? "Win32" : "Win64";
-#endif
-    }
-
-    bool IsWindowsPlatform(ReadOnlyTargetRules Target)
-    {
-        bool bWindows =
-#if !UE_5_0_OR_LATER
-            Target.Platform == UnrealTargetPlatform.Win32 ||
-#endif
-            Target.Platform == UnrealTargetPlatform.Win64;
-
-        return bWindows;
-    }
-
     public QTMConnectLiveLink(ReadOnlyTargetRules Target) : base(Target)
     {
         PCHUsage = ModuleRules.PCHUsageMode.UseExplicitOrSharedPCHs;
         string pluginPublicIncludePath = Path.Combine(ModuleDirectory, "Public");
         string pluginPrivateIncludePath = Path.Combine(ModuleDirectory, "Private");
-
-        string targetPlatformPathName = GetTargetPlatformPathName(Target);
 
         PublicIncludePaths.AddRange(
             new string[] {
