8e995beca884e80866e9c31cff91c6c5b3cc315c
qjgn <45095781+qjgn@users.noreply.github.com>
2025-02-24 13:17:29 +0100

    Updated qualisys_cpp_sdk and init scripts (#71)
    Issue:
    	The inclusion of tinyxml2 generates warning C4459
    	Engine\Source\Runtime\Core\Public\Math\RotationTranslationMatrix.h(60):
    		error C4459: declaration of 'CR' hides global declaration
    
    Solution:
    	Disabled shadow variable warning in QTMConnectLiveLink.Build.cs

--- qualisys_cpp_sdk_init.bat
+++ qualisys_cpp_sdk_init.bat
@@ -1,20 +1,33 @@
 @echo off
 
 set "dir_private=Qualisys\QTMConnectLiveLink\Source\QTMConnectLiveLink\Private\RTClientSDK"
-
-echo Ensuring directories exist...
-if not exist "%dir_private%" (
-    mkdir "%dir_private%"
-    echo Created %dir_private%
-)
-
+echo Ensuring directory exists...
+if not exist "%dir_private%" mkdir "%dir_private%"
 echo Copying files from qualisys_cpp_sdk into %dir_private%
-copy /Y "qualisys_cpp_sdk\Markup.cpp" "%dir_private%"
-copy /Y "qualisys_cpp_sdk\Network.cpp" "%dir_private%"
-copy /Y "qualisys_cpp_sdk\RTPacket.cpp" "%dir_private%"
-copy /Y "qualisys_cpp_sdk\RTProtocol.cpp" "%dir_private%"
-copy /Y "qualisys_cpp_sdk\Markup.h" "%dir_private%"
-copy /Y "qualisys_cpp_sdk\Network.h" "%dir_private%"
-copy /Y "qualisys_cpp_sdk\RTPacket.h" "%dir_private%"
-copy /Y "qualisys_cpp_sdk\RTProtocol.h" "%dir_private%"
+xcopy /Y "qualisys_cpp_sdk\Deserializer.cpp" "%dir_private%\"
+xcopy /Y "qualisys_cpp_sdk\Deserializer.h" "%dir_private%\"
+xcopy /Y "qualisys_cpp_sdk\LICENSE.md" "%dir_private%\"
+xcopy /Y "qualisys_cpp_sdk\Network.cpp" "%dir_private%\"
+xcopy /Y "qualisys_cpp_sdk\Network.h" "%dir_private%\"
+xcopy /Y "qualisys_cpp_sdk\RTPacket.cpp" "%dir_private%\"
+xcopy /Y "qualisys_cpp_sdk\RTPacket.h" "%dir_private%\"
+xcopy /Y "qualisys_cpp_sdk\RTProtocol.cpp" "%dir_private%\"
+xcopy /Y "qualisys_cpp_sdk\RTProtocol.h" "%dir_private%\"
+xcopy /Y "qualisys_cpp_sdk\Serializer.cpp" "%dir_private%\"
+xcopy /Y "qualisys_cpp_sdk\Serializer.h" "%dir_private%\"
+xcopy /Y "qualisys_cpp_sdk\Settings.cpp" "%dir_private%\"
+xcopy /Y "qualisys_cpp_sdk\Settings.h" "%dir_private%\"
+xcopy /Y "qualisys_cpp_sdk\SettingsDeserializer.cpp" "%dir_private%\"
+xcopy /Y "qualisys_cpp_sdk\SettingsDeserializer.h" "%dir_private%\"
+xcopy /Y "qualisys_cpp_sdk\SettingsSerializer.cpp" "%dir_private%\"
+xcopy /Y "qualisys_cpp_sdk\SettingsSerializer.h" "%dir_private%\"
 echo Done copying files from qualisys_cpp_sdk into %dir_private%
+
+set "tinyxml2_private=Qualisys\QTMConnectLiveLink\Source\QTMConnectLiveLink\Private\RTClientSDK\External\tinyxml2"
+echo Ensuring directory exists...
+if not exist "%tinyxml2_private%" mkdir "%tinyxml2_private%"
+echo Copying files from qualisys_cpp_sdk into %tinyxml2_private%
+xcopy /Y "qualisys_cpp_sdk\External\tinyxml2\LICENSE.txt" "%tinyxml2_private%\"
+xcopy /Y "qualisys_cpp_sdk\External\tinyxml2\tinyxml2.cpp" "%tinyxml2_private%\"
+xcopy /Y "qualisys_cpp_sdk\External\tinyxml2\tinyxml2.h" "%tinyxml2_private%\"
+echo Done copying files from qualisys_cpp_sdk into %tinyxml2_private%
